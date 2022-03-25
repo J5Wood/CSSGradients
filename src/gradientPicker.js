24bf0db10ba50d03bf2f4265ce5b9f4ff8aafb96
Jeremy Wood <68167282+J5Wood@users.noreply.github.com>
2022-03-25 00:04:44 -0700

    Fixed some color form and list corelation functionality

--- src/gradientPicker.js
+++ src/gradientPicker.js
@@ -1,149 +1,159 @@
 // ***** Display and input section
 
-
-
 // ***** Each list item needs to have option for one or two degree values between 0 and 100
 // ***** Maybe a slider later? Start with input boxes.
 // ***** Need degree adjuster up top, one adjuster for all values
 
+const colorDisplay = document.querySelector(".selection-display");
 
-const colorDisplay = document.querySelector(".selection-display")
+const hexInput = document.querySelector(".hex-value");
+const redInput = document.querySelector(".red");
+const blueInput = document.querySelector(".blue");
+const greenInput = document.querySelector(".green");
+const opacityInput = document.querySelector(".opacity-number");
+const gradientList = document.querySelector(".gradient-list");
+let red = 0;
+let green = 255;
+let blue = 0;
+let opacity = 1;
 
-const hexInput = document.querySelector(".hex-value")
-const redInput = document.querySelector(".red")
-const blueInput = document.querySelector(".blue")
-const greenInput = document.querySelector(".green")
-const opacityInput = document.querySelector(".opacity-number")
-const gradientList = document.querySelector(".gradient-list")
-let red = 0
-let green = 255
-let blue = 0
-let opacity = 1
+redInput.addEventListener("change", (e) => handleColorChange(e));
+blueInput.addEventListener("change", (e) => handleColorChange(e));
+greenInput.addEventListener("change", (e) => handleColorChange(e));
+opacityInput.addEventListener("change", (e) => handleColorChange(e));
+hexInput.addEventListener("change", (e) => handleHexChange(e.target.value));
+gradientList.addEventListener("change", (e) => handleGradientChange(e));
 
-redInput.addEventListener("change", e => handleColorChange(e))
-blueInput.addEventListener("change", e => handleColorChange(e))
-greenInput.addEventListener("change", e => handleColorChange(e))
-opacityInput.addEventListener("change", e => handleColorChange(e))
-hexInput.addEventListener("change", e => handleHexChange(e))
-gradientList.addEventListener("change", e => handleGradientChange(e))
+redInput.addEventListener("keyup", (e) => handleColorChange(e));
+blueInput.addEventListener("keyup", (e) => handleColorChange(e));
+greenInput.addEventListener("keyup", (e) => handleColorChange(e));
+opacityInput.addEventListener("keyup", (e) => handleColorChange(e));
 
-redInput.addEventListener("keyup", e => handleColorChange(e))
-blueInput.addEventListener("keyup", e => handleColorChange(e))
-greenInput.addEventListener("keyup", e => handleColorChange(e))
-opacityInput.addEventListener("keyup", e => handleColorChange(e))
-
-redInput.addEventListener("blur", e => handleEmptyDeselection(e))
-blueInput.addEventListener("blur", e => handleEmptyDeselection(e))
-greenInput.addEventListener("blur", e => handleEmptyDeselection(e))
-opacityInput.addEventListener("blur", e => handleEmptyDeselection(e))
+redInput.addEventListener("blur", (e) => handleEmptyDeselection(e));
+blueInput.addEventListener("blur", (e) => handleEmptyDeselection(e));
+greenInput.addEventListener("blur", (e) => handleEmptyDeselection(e));
+opacityInput.addEventListener("blur", (e) => handleEmptyDeselection(e));
 
 function handleColorChange(e) {
-    if(e.target.value > 255) e.target.value = 255
-    if(e.target.value < 0) e.target.value = 0
-    if(e.target.name === "red"){
-        red = e.target.value
-    } else if(e.target.name === "blue"){
-        blue = e.target.value
-    } else if(e.target.name === "green"){
-        green = e.target.value
-    } else {
-        opacity = e.target.value
-    }
+  if (e.target.value > 255) e.target.value = 255;
+  if (e.target.value < 0 || e.target.value.match(/[^0-9.]/)) e.target.value = 0;
+  if (e.target.name === "red") {
+    red = e.target.value;
+  } else if (e.target.name === "blue") {
+    blue = e.target.value;
+  } else if (e.target.name === "green") {
+    green = e.target.value;
+  } else {
+    opacity = e.target.value;
+  }
 
-    const hexValArr = [parseInt(red).toString(16), parseInt(green).toString(16), parseInt(blue).toString(16)]
-    for(let i = 0; i < hexValArr.length; i++){
-        if(hexValArr[i] === "NaN") hexValArr[i] = "00"
-        hexValArr[i] = hexValArr[i].padStart(2, "0")
-    }
-    hexInput.value = hexValArr.join("")
+  const hexValArr = [
+    parseInt(red).toString(16),
+    parseInt(green).toString(16),
+    parseInt(blue).toString(16),
+  ];
+  for (let i = 0; i < hexValArr.length; i++) {
+    if (hexValArr[i] === "NaN") hexValArr[i] = "00";
+    hexValArr[i] = hexValArr[i].padStart(2, "0");
+  }
+  hexInput.value = hexValArr.join("");
 
-    colorDisplay.style.background = currentColor.firstElementChild.style.background = `rgba(${red}, ${green}, ${blue}, ${opacity})`
+  colorDisplay.style.background =
+    currentColor.firstElementChild.style.background = `rgba(${red}, ${green}, ${blue}, ${opacity})`;
 
-    currentColor.getElementsByTagName("input")[0].value = hexValArr.join("")
+  currentColor.getElementsByTagName("input")[0].value = `#${hexValArr.join(
+    ""
+  )}`;
 }
 
-function handleHexChange(e) {
-    let numValArr
-    if(!!e.target.value.match(/[^0-9a-fA-F]+/gm)){
-        numValArr = [0, 0, 0]
-        hexInput.value = "000000"
-    } else{
-        if(e.target.value.length < 6) {
-            e.target.value = e.target.value.padStart(6, "0")
-        }
-        let numArr = e.target.value.split(/(.{2})/gm).filter(val => !!val === true)
-        numValArr = numArr.map(num => parseInt(num, 16))
+function handleHexChange(value) {
+  let numValArr;
+  if (!!value.match(/[^0-9a-fA-F]+/gm)) {
+    numValArr = [0, 0, 0];
+    hexInput.value = "000000";
+  } else {
+    if (value.length < 6) {
+      value = value.padStart(6, "0");
     }
-    red = redInput.value = numValArr[0]
-    green = greenInput.value = numValArr[1]
-    blue = blueInput.value = numValArr[2]
+    let numArr = value.split(/(.{2})/gm).filter((val) => !!val === true);
+    numValArr = numArr.map((num) => parseInt(num, 16));
+  }
+  red = redInput.value = numValArr[0];
+  green = greenInput.value = numValArr[1];
+  blue = blueInput.value = numValArr[2];
 
-    colorDisplay.style.background = currentColor.firstElementChild.style.background = `rgba(${red}, ${green}, ${blue}, ${opacity})`
-    currentColor.getElementsByTagName("input")[0].value = e.target.value
+  colorDisplay.style.background =
+    currentColor.firstElementChild.style.background = `rgba(${red}, ${green}, ${blue}, ${opacity})`;
+  currentColor.getElementsByTagName("input")[0].value = `#${value}`;
 }
 
-function handleEmptyDeselection(e){
-    if(!e.target.value) {
-        e.target.value = "0"
-        handleColorChange(e)
-    }
+function handleEmptyDeselection(e) {
+  if (!e.target.value) {
+    e.target.value = "0";
+    handleColorChange(e);
+  }
 }
 
-function handleGradientChange(e){
-    console.log(e.target.value)
-    debugger
-    colorDisplay.style.background = `${e.target.value}-gradient(0deg, rgba(${red}, ${green}, ${blue}, ${opacity}), rgba(${red}, ${green}, ${blue}, ${opacity}))`
+function handleGradientChange(e) {
+  console.log(e.target.value);
+  debugger;
+  colorDisplay.style.background = `${e.target.value}-gradient(0deg, rgba(${red}, ${green}, ${blue}, ${opacity}), rgba(${red}, ${green}, ${blue}, ${opacity}))`;
 }
 
 // ***** List section
-const colorList = document.querySelector(".color-list")
-const colors = document.getElementsByClassName("color")
-let currentColor = colors[0]
-currentColor.addEventListener("click", e => handleColorSelection(e))
-currentColor.lastElementChild.addEventListener("click", e => removeColor(e))
+const colorList = document.querySelector(".color-list");
+const colors = document.getElementsByClassName("color");
+let currentColor = colors[0];
+currentColor.children[0].style.backgroundColor = "#00ff00";
+currentColor.addEventListener("click", (e) => handleColorSelection(e));
+currentColor.lastElementChild.addEventListener("click", (e) => removeColor(e));
 
-function handleColorSelection(e){
-    currentColor.classList.remove("selected-color")
-    if(e.target.className === 'color'){
-    currentColor = e.target
-    } else {
-        currentColor = e.target.parentElement
-    }
-    currentColor.classList.add("selected-color")
+function handleColorSelection(e) {
+  currentColor.classList.remove("selected-color");
+  if (e.target.className === "color") {
+    currentColor = e.target;
+  } else {
+    currentColor = e.target.parentElement;
+  }
+  currentColor.classList.add("selected-color");
+
+  let hexColor = currentColor.children[2].value.slice(1);
+  hexInput.value = hexColor;
+
+  handleHexChange(hexColor);
 }
 
+const addColorButton = document.querySelector(".add-color-button");
+addColorButton.addEventListener("click", addNewColor);
 
-const addColorButton = document.querySelector(".add-color-button")
-addColorButton.addEventListener("click", addNewColor)
+function addNewColor() {
+  const newItem = document.createElement("li");
+  newItem.classList.add("color");
 
-function addNewColor(){
-    const newItem = document.createElement("li")
-    newItem.classList.add("color")
+  const span = document.createElement("span");
+  span.classList.add("color-sample");
 
-    const span = document.createElement("span")
-    span.classList.add("color-sample")
+  const h3 = document.createElement("h3");
+  h3.innerText = "Color Sample 1";
 
-    const h3 = document.createElement("h3")
-    h3.innerText = "Color Sample 1"
+  const input = document.createElement("input");
+  input.setAttribute("name", "color-input");
+  input.setAttribute("value", "#000000");
 
-    const input = document.createElement("input")
-    input.setAttribute("name", "color-input")
-    input.setAttribute("value", "#000000")
+  const button = document.createElement("button");
+  button.classList.add("delete-color-button");
+  button.innerText = "X";
+  button.addEventListener("click", (e) => removeColor(e));
 
-    const button = document.createElement("button")
-    button.classList.add("delete-color-button")
-    button.innerText = "X"
-    button.addEventListener("click", e => removeColor(e))
-
-    newItem.appendChild(span)
-    newItem.appendChild(h3)
-    newItem.appendChild(input)
-    newItem.appendChild(button)
-    newItem.addEventListener("click", e => handleColorSelection(e))
-    colorList.insertBefore(newItem, colorList.lastElementChild)
+  newItem.appendChild(span);
+  newItem.appendChild(h3);
+  newItem.appendChild(input);
+  newItem.appendChild(button);
+  newItem.addEventListener("click", (e) => handleColorSelection(e));
+  colorList.insertBefore(newItem, colorList.lastElementChild);
 }
 
-function removeColor(e){
-    e.target.parentElement.remove()
-    e.stopPropagation()
+function removeColor(e) {
+  e.target.parentElement.remove();
+  e.stopPropagation();
 }
